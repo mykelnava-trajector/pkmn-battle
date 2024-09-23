f734bc99b0e9b796f0a956534209ce8209f4e9dd
Mykel Angelo P. Nava <MykelAngelo.Nava@trajectorservices.com>
2024-09-24 03:17:50 +0800

    Final Pokemon Express Activity to be checked

--- PokemonBattle.ts
+++ PokemonBattle.ts
@@ -45,39 +45,39 @@ const clearPokemon = () => {
 }
 
 app.get(`/`, (req,res) =>{
-    res.send(`Store Your Pokemon
-        <p>URL: http://localhost:3000/storeyourpokemon?ypokemon=(pokemonname)</p>
-        <p>Store Enemy Pokemon</p>
-        <p>URL: http://localhost:3000/storeenemypokemon?epokemon=(pokemonname)</p>
-        <p>Pokemon Battle (Must choose both your Pokemon and enemy Pokemon before choosing this feauture)</p>
-        <p>http://localhost:3000/pokemonbattle</p>`)
+    res.send(`<b>Pokemon Menu with Links</b>
+        <p><b>Store Your Pokemon</b></p>
+        <p><b>URL: http://localhost:3000/storeyourpokemon?ypokemon=(pokemonname)</b></p>
+        <p><b>Store Enemy Pokemon</b></p>
+        <p><b>URL: http://localhost:3000/storeenemypokemon?epokemon=(pokemonname)</b></p>
+        <p><b>Pokemon Battle (Must choose both your Pokemon and enemy Pokemon before choosing this feauture)</b></p>
+        <p><b>URL: http://localhost:3000/pokemonbattle</b></p>`)
 })
 
 app.get(`/storeyourpokemon`, async(req,res) =>{
     let ypokemon = req.query.ypokemon
-    yourPoke(ypokemon)
-    if(ypokemon){
-        res.send(`Your pokemon ${ypokemon} has been added to the battle.`)
-    }
-    else{
-        res.send(`Error. Pick again.`)
-        yourPoke(ypokemon)
+    try {
+        await yourPoke(ypokemon);
+        res.send(`<b>Your Pokémon ${ypokemon} has been added to the battle.</b>`);
+    } catch (error) {
+        res.status(400).send(`Error: ${error.message}. Please try again.`);
     }
 })
 
 app.get(`/storeenemypokemon`, async(req,res) => {
     let epokemon = req.query.epokemon
-    enemyPoke(epokemon)
-    if(epokemon){
-        res.send(`Enemy pokemon ${epokemon} has been added to the battle.`)
-    }
-    else{
-        res.send(`Error. Pick again.`)
-        enemyPoke(epokemon)
+    try{
+        await enemyPoke(epokemon);
+        res.send(`<b>Enemy Pokémon ${epokemon} has been added to the battle.</b>`);
+    } catch (error) {
+        res.status(400).send(`Error: ${error.message}. Please try again.`);
     }
 })
 
 app.get(`/pokemonbattle`, (req,res) => {
+    if (Pokestats1.length === 0 || Pokestats2.length === 0) {
+        res.status(400).send('Error: You must choose both your Pokémon and an enemy Pokémon before battling.')
+    }
     let yourPokemonName = Pokestats1[0].name
     let yourPokemonMoveName = Pokestats1[0].move
     let yourPokemonHP = Pokestats1[0].hp
@@ -86,56 +86,53 @@ app.get(`/pokemonbattle`, (req,res) => {
     let enemyPokemonMoveName = Pokestats2[0].move
     let enemyPokemonHP = Pokestats2[0].hp
     let enemyPokemonDMG = Pokestats2[0].dmg
-        if (!Pokestats1.length || !Pokestats2.length) {
-        return res.send(`Choose pokemons before going to this endpoint.`);
-            }
         if(yourPokemonName && yourPokemonDMG && yourPokemonHP && yourPokemonMoveName
         && enemyPokemonName && enemyPokemonDMG && enemyPokemonHP && enemyPokemonMoveName){
             let yourPokemonHPDamaged = yourPokemonHP - enemyPokemonDMG
             let enemyPokemonHPDamaged = enemyPokemonHP - yourPokemonDMG
             if(yourPokemonHPDamaged > 0 && enemyPokemonHPDamaged <= 0 || yourPokemonHPDamaged > enemyPokemonHPDamaged){
-                res.send(`Your Pokemon Name: ${yourPokemonName}
-                    <p>Your Pokemon Move Name: ${yourPokemonMoveName}</p>
-                    <p>Your Pokemon HP: ${yourPokemonHP}</p>
-                    <p>Your Pokemon DMG: ${yourPokemonDMG}
-                    <p>================================================</p>
-                    <p>Enemy Pokemon: ${enemyPokemonName}</p>
-                    <p>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</p>
-                    <p>Enemy Pokemon HP: ${enemyPokemonHP}</p>
-                    <p>Enemy Pokemon DMG: ${enemyPokemonDMG}</p>
-                    <p>================================================</p>
-                    <p> Your pokemon won! </p>
-                    <p> Head back to <a href="http://localhost:3000">http://localhost:3000</a></p>`)
+                res.send(`<b>Your Pokemon Name: ${yourPokemonName}</b>
+                    <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
+                    <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
+                    <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
+                    <p><b>================================================</b></p>
+                    <p><b>Enemy Pokemon: ${enemyPokemonName}</b></p>
+                    <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</b></p>
+                    <p><b>Enemy Pokemon HP: ${enemyPokemonHP}</b></p>
+                    <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}</b></p>
+                    <p><b>================================================</b></p>
+                    <p><b>Your pokemon won!</b></p>
+                    <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a></b></p>`)
                     clearPokemon()
                 }
             else if(enemyPokemonHPDamaged > 0 && yourPokemonHPDamaged <= 0 || enemyPokemonHPDamaged > yourPokemonHPDamaged){
-                res.send(`Your Pokemon Name: ${yourPokemonName}
-                    <p>Your Pokemon Move Name: ${yourPokemonMoveName}</p>
-                    <p>Your Pokemon HP: ${yourPokemonHP}</p>
-                    <p>Your Pokemon DMG: ${yourPokemonDMG}
-                    <p>================================================</p>
-                    <p>Enemy Pokemon: ${enemyPokemonName}</p>
-                    <p>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</p>
-                    <p>Enemy Pokemon HP: ${enemyPokemonHP}</p>
-                    <p>Enemy Pokemon DMG: ${enemyPokemonDMG}</p>
-                    <p>================================================</p>
-                    <p> Enemy pokemon won! </p>
-                    <p> Head back to <a href="http://localhost:3000">http://localhost:3000</a></p>`)
+                res.send(`<b>Your Pokemon Name: ${yourPokemonName}</b>
+                    <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
+                    <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
+                    <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
+                    <p><b>================================================</b></p>
+                    <p><b>Enemy Pokemon: ${enemyPokemonName}</b></p>
+                    <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</b></p>
+                    <p><b>Enemy Pokemon HP: ${enemyPokemonHP}</b></p>
+                    <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}</b></p>
+                    <p><b>================================================</p>
+                    <p><b>Enemy pokemon won!</b></p>
+                    <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a></p>`)
                     clearPokemon()
                 }
             else if(yourPokemonHPDamaged <= 0 && enemyPokemonHPDamaged <= 0 || yourPokemonHPDamaged == enemyPokemonHPDamaged){
-                res.send(`Your Pokemon Name: ${yourPokemonName}
-                    <p>Your Pokemon Move Name: ${yourPokemonMoveName}</p>
-                    <p>Your Pokemon HP: ${yourPokemonHP}</p>
-                    <p>Your Pokemon DMG: ${yourPokemonDMG}
-                    <p>================================================</p>
-                    <p>Enemy Pokemon: ${enemyPokemonName}</p>
-                    <p>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</p>
-                    <p>Enemy Pokemon HP: ${enemyPokemonHP}</p>
-                    <p>Enemy Pokemon DMG: ${enemyPokemonDMG}</p>
-                    <p>================================================</p>
-                    <p> It's a draw! No one won! </p>
-                    <p>Head back to <a href="http://localhost:3000">http://localhost:3000</a></p>`)
+                res.send(`<b>Your Pokemon Name: ${yourPokemonName}</b>
+                    <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
+                    <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
+                    <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
+                    <p><b>================================================</b></p>
+                    <p><b>Enemy Pokemon: ${enemyPokemonName}}</b></p>
+                    <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}}</b></p>
+                    <p><b>Enemy Pokemon HP: ${enemyPokemonHP}}</b></p>
+                    <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}}</b></p>
+                    <p><b>================================================}</b></p>
+                    <p><b>It's a draw! No one won!}</b></p>
+                    <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a>}</b></p>`)
                     clearPokemon()
             }
     }
