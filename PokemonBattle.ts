c5500786a0297dafa03eb749041848513d8ed08f
Mykel Angelo P. Nava <MykelAngelo.Nava@trajectorservices.com>
2024-09-24 04:59:42 +0800

    Added images for the final output

--- PokemonBattle.ts
+++ PokemonBattle.ts
@@ -9,6 +9,7 @@ interface PokemonStats{
     move: string;
     hp: number;
     dmg: number;
+    image: string;
 }
 
 let Pokestats1 : PokemonStats[] = []
@@ -18,24 +19,33 @@ let Pokestats2 : PokemonStats[] = []
 const yourPoke = async(YPokemonP: string) => {
         const PokeAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${YPokemonP}`)
         const PokeAPIJSON = await PokeAPI.json()
+        const PokemonAPIImage = await fetch(PokeAPIJSON.sprites.front_default);
+        const image = await PokemonAPIImage.arrayBuffer();
+        const imageBase64 = Buffer.from(image).toString('base64');
         let PStats1: PokemonStats = {
             name: PokeAPIJSON.name,
             move: PokeAPIJSON.moves[0].move.name,
             hp: PokeAPIJSON.stats[0].base_stat,
-            dmg: PokeAPIJSON.stats[1].base_stat
+            dmg: PokeAPIJSON.stats[1].base_stat,
+            image: imageBase64
             }
         Pokestats1.push(PStats1)    
         }
 
 const enemyPoke = async(EPokemonP: string) => {
     const EPokeAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${EPokemonP}`)
-        const EPokeAPIJSON = await EPokeAPI.json()
+    const EPokeAPIJSON = await EPokeAPI.json()
+    const EPokemonAPIImage = await fetch(EPokeAPIJSON.sprites.front_default);
+    const Eimage = await EPokemonAPIImage.arrayBuffer();
+    const EimageBase64 = Buffer.from(Eimage).toString('base64');
         let PStats2: PokemonStats = {
             name: EPokeAPIJSON.name,
             move: EPokeAPIJSON.moves[0].move.name,
             hp: EPokeAPIJSON.stats[0].base_stat,
-            dmg: EPokeAPIJSON.stats[1].base_stat
+            dmg: EPokeAPIJSON.stats[1].base_stat,
+            image: EimageBase64
             }
+        
         Pokestats2.push(PStats2)   
 }
 
@@ -82,10 +92,12 @@ app.get(`/pokemonbattle`, (req,res) => {
     let yourPokemonMoveName = Pokestats1[0].move
     let yourPokemonHP = Pokestats1[0].hp
     let yourPokemonDMG = Pokestats1[0].dmg
+    let yourPokemonImage = Pokestats1[0].image
     let enemyPokemonName = Pokestats2[0].name
     let enemyPokemonMoveName = Pokestats2[0].move
     let enemyPokemonHP = Pokestats2[0].hp
     let enemyPokemonDMG = Pokestats2[0].dmg
+    let enemyPokemonImage = Pokestats2[0].image
         if(yourPokemonName && yourPokemonDMG && yourPokemonHP && yourPokemonMoveName
         && enemyPokemonName && enemyPokemonDMG && enemyPokemonHP && enemyPokemonMoveName){
             let yourPokemonHPDamaged = yourPokemonHP - enemyPokemonDMG
@@ -95,11 +107,13 @@ app.get(`/pokemonbattle`, (req,res) => {
                     <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
                     <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
                     <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
+                    <p><b>Your Pokemon image: <img src="data:image/png;base64,${yourPokemonImage}"></b></p>
                     <p><b>================================================</b></p>
                     <p><b>Enemy Pokemon: ${enemyPokemonName}</b></p>
                     <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</b></p>
                     <p><b>Enemy Pokemon HP: ${enemyPokemonHP}</b></p>
                     <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}</b></p>
+                    <p><b>Enemy Pokemon image: <img src="data:image/png;base64,${enemyPokemonImage}"></b></p>
                     <p><b>================================================</b></p>
                     <p><b>Your pokemon won!</b></p>
                     <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a></b></p>`)
@@ -110,11 +124,13 @@ app.get(`/pokemonbattle`, (req,res) => {
                     <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
                     <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
                     <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
+                    <p><b>Your Pokemon image: <img src="data:image/png;base64,${yourPokemonImage}"></b></p>
                     <p><b>================================================</b></p>
                     <p><b>Enemy Pokemon: ${enemyPokemonName}</b></p>
                     <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</b></p>
                     <p><b>Enemy Pokemon HP: ${enemyPokemonHP}</b></p>
                     <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}</b></p>
+                    <p><b>Enemy Pokemon image: <img src="data:image/png;base64,${enemyPokemonImage}"></b></p>
                     <p><b>================================================</p>
                     <p><b>Enemy pokemon won!</b></p>
                     <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a></p>`)
@@ -125,11 +141,13 @@ app.get(`/pokemonbattle`, (req,res) => {
                     <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
                     <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
                     <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
+                    <p><b>Your Pokemon image: <img src="data:image/png;base64,${yourPokemonImage}"></b></p>
                     <p><b>================================================</b></p>
                     <p><b>Enemy Pokemon: ${enemyPokemonName}}</b></p>
                     <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}}</b></p>
                     <p><b>Enemy Pokemon HP: ${enemyPokemonHP}}</b></p>
                     <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}}</b></p>
+                     <p><b>Enemy Pokemon image: <img src="data:image/png;base64,${enemyPokemonImage}"></b></p>
                     <p><b>================================================}</b></p>
                     <p><b>It's a draw! No one won!}</b></p>
                     <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a>}</b></p>`)
