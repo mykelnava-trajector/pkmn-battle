8c7cc521da7893e485828886abd83ab86f42b7d1
Mykel Angelo P. Nava <MykelAngelo.Nava@trajectorservices.com>
2024-09-25 01:23:49 +0800

    Made picking pokemons and battle into JSON.

--- PokemonBattle.ts
+++ PokemonBattle.ts
@@ -9,8 +9,6 @@ interface PokemonStats{
     move: string;
     hp: number;
     dmg: number;
-    image: string;
-    sound: string;
 }
 
 let Pokestats1 : PokemonStats[] = []
@@ -20,19 +18,11 @@ let Pokestats2 : PokemonStats[] = []
 const yourPoke = async(YPokemonP: string) => {
         const PokeAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${YPokemonP}`)
         const PokeAPIJSON = await PokeAPI.json()
-        const PokemonAPIImage = await fetch(PokeAPIJSON.sprites.front_default);
-        const image = await PokemonAPIImage.arrayBuffer();
-        const imageBase64 = Buffer.from(image).toString('base64');
-        const PokemonAPISound = await fetch(PokeAPIJSON.cries.latest)
-        const sound = await PokemonAPISound.arrayBuffer();
-        const soundBase64 = Buffer.from(sound).toString('base64');
         let PStats1: PokemonStats = {
             name: PokeAPIJSON.name,
             move: PokeAPIJSON.moves[0].move.name,
             hp: PokeAPIJSON.stats[0].base_stat,
-            dmg: PokeAPIJSON.stats[1].base_stat,
-            image: imageBase64,
-            sound: `data:audio/wav;base64,${soundBase64}`
+            dmg: PokeAPIJSON.stats[1].base_stat
             }
         Pokestats1.push(PStats1)    
         }
@@ -40,21 +30,12 @@ const yourPoke = async(YPokemonP: string) => {
 const enemyPoke = async(EPokemonP: string) => {
     const EPokeAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${EPokemonP}`)
     const EPokeAPIJSON = await EPokeAPI.json()
-    const EPokemonAPIImage = await fetch(EPokeAPIJSON.sprites.front_default);
-    const Eimage = await EPokemonAPIImage.arrayBuffer();
-    const EimageBase64 = Buffer.from(Eimage).toString('base64');
-    const EPokemonAPISound = await fetch(EPokeAPIJSON.cries.latest)
-    const Esound = await EPokemonAPISound.arrayBuffer();
-    const EsoundBase64 = Buffer.from(Esound).toString('base64');
         let PStats2: PokemonStats = {
             name: EPokeAPIJSON.name,
             move: EPokeAPIJSON.moves[0].move.name,
             hp: EPokeAPIJSON.stats[0].base_stat,
-            dmg: EPokeAPIJSON.stats[1].base_stat,
-            image: EimageBase64,
-            sound:`data:audio/wav;base64, ${EsoundBase64}`
+            dmg: EPokeAPIJSON.stats[1].base_stat
             }
-        
         Pokestats2.push(PStats2)   
 }
 
@@ -77,7 +58,7 @@ app.get(`/storeyourpokemon`, async(req,res) =>{
     let ypokemon = req.query.ypokemon
     try {
         await yourPoke(ypokemon);
-        res.send(`<b>Your Pokémon ${ypokemon} has been added to the battle.</b>`);
+        res.json({YourPokemonRegistered:Pokestats1})
     } catch (error) {
         res.status(400).send(`Error: ${error.message}. Please try again.`);
     }
@@ -87,7 +68,7 @@ app.get(`/storeenemypokemon`, async(req,res) => {
     let epokemon = req.query.epokemon
     try{
         await enemyPoke(epokemon);
-        res.send(`<b>Enemy Pokémon ${epokemon} has been added to the battle.</b>`);
+        res.json({EnemyPokemonRegistered:Pokestats2})
     } catch (error) {
         res.status(400).send(`Error: ${error.message}. Please try again.`);
     }
@@ -101,92 +82,63 @@ app.get(`/pokemonbattle`, (req,res) => {
     let yourPokemonMoveName = Pokestats1[0].move
     let yourPokemonHP = Pokestats1[0].hp
     let yourPokemonDMG = Pokestats1[0].dmg
-    let yourPokemonImage = Pokestats1[0].image
-    let yourPokemonSound = Pokestats1[0].sound
     let enemyPokemonName = Pokestats2[0].name
     let enemyPokemonMoveName = Pokestats2[0].move
     let enemyPokemonHP = Pokestats2[0].hp
     let enemyPokemonDMG = Pokestats2[0].dmg
-    let enemyPokemonImage = Pokestats2[0].image
-    let enemyPokemonSound = Pokestats2[0].sound
         if(yourPokemonName && yourPokemonDMG && yourPokemonHP && yourPokemonMoveName
         && enemyPokemonName && enemyPokemonDMG && enemyPokemonHP && enemyPokemonMoveName){
             let yourPokemonHPDamaged = yourPokemonHP - enemyPokemonDMG
             let enemyPokemonHPDamaged = enemyPokemonHP - yourPokemonDMG
             if(yourPokemonHPDamaged > 0 && enemyPokemonHPDamaged <= 0 || yourPokemonHPDamaged > enemyPokemonHPDamaged){
-                res.send(`<b>Your Pokemon Name: ${yourPokemonName}</b>
-                    <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
-                    <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
-                    <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
-                    <p><b>Your Pokemon image: <img src="data:image/png;base64,${yourPokemonImage}"></b></p>
-                    <p><audio controls>
-                    <source src ="${yourPokemonSound}" type="audio/wav">
-                    Your Browser doesn't support the audio element.
-                    </audio></p>
-                    <p><b>================================================</b></p>
-                    <p><b>Enemy Pokemon: ${enemyPokemonName}</b></p>
-                    <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</b></p>
-                    <p><b>Enemy Pokemon HP: ${enemyPokemonHP}</b></p>
-                    <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}</b></p>
-                    <p><b>Enemy Pokemon image: <img src="data:image/png;base64,${enemyPokemonImage}"></b></p>
-                    <p><audio controls>
-                    <source src ="${enemyPokemonSound}" type="audio/wav">
-                    Your Browser doesn't support the audio element.
-                    </audio></p>
-                    <p><b>================================================</b></p>
-                    <p><b>Your pokemon won!</b></p>
-                    <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a></b></p>`)
+                res.json({
+                    winner:yourPokemonName,
+                    YourPokemon:{
+                        name: yourPokemonName,
+                        attack: yourPokemonDMG,
+                        hp: yourPokemonHP
+                    },
+                    EnemyPokemon:{
+                        name:enemyPokemonName,
+                        attack: enemyPokemonDMG,
+                        hp: enemyPokemonHP
+                    },
+                    results: "Your Pokemon Won"
+                })
                     clearPokemon()
                 }
             else if(enemyPokemonHPDamaged > 0 && yourPokemonHPDamaged <= 0 || enemyPokemonHPDamaged > yourPokemonHPDamaged){
-                res.send(`<b>Your Pokemon Name: ${yourPokemonName}</b>
-                    <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
-                    <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
-                    <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
-                    <p><b>Your Pokemon image: <img src="data:image/png;base64,${yourPokemonImage}"></b></p>
-                    <p><audio controls>
-                    <source src ="${yourPokemonSound}" type="audio/wav">
-                    Your Browser doesn't support the audio element.
-                    </audio></p>
-                    <p><b>================================================</b></p>
-                    <p><b>Enemy Pokemon: ${enemyPokemonName}</b></p>
-                    <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}</b></p>
-                    <p><b>Enemy Pokemon HP: ${enemyPokemonHP}</b></p>
-                    <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}</b></p>
-                    <p><b>Enemy Pokemon image: <img src="data:image/png;base64,${enemyPokemonImage}"></b></p>
-                    <p><audio controls>
-                    <source src ="${enemyPokemonSound}" type="audio/wav">
-                    Your Browser doesn't support the audio element.
-                    </audio></p>
-                    <p><b>================================================</p>
-                    <p><b>Enemy pokemon won!</b></p>
-                    <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a></p>`)
+                res.json({
+                    winner:enemyPokemonName,
+                    YourPokemon:{
+                        name: yourPokemonName,
+                        attack: yourPokemonDMG,
+                        hp: yourPokemonHP
+                    },
+                    EnemyPokemon:{
+                        name:enemyPokemonName,
+                        attack: enemyPokemonDMG,
+                        hp: enemyPokemonHP
+                    },
+                    results: "Enemy Pokemon Won"
+                })
                     clearPokemon()
                 }
             else if(yourPokemonHPDamaged <= 0 && enemyPokemonHPDamaged <= 0 || yourPokemonHPDamaged == enemyPokemonHPDamaged){
-                res.send(`<b>Your Pokemon Name: ${yourPokemonName}</b>
-                    <p><b>Your Pokemon Move Name: ${yourPokemonMoveName}</b></p>
-                    <p><b>Your Pokemon HP: ${yourPokemonHP}</b></p>
-                    <p><b>Your Pokemon DMG: ${yourPokemonDMG}</b></p>
-                    <p><b>Your Pokemon image: <img src="data:image/png;base64,${yourPokemonImage}"></b></p>
-                    <p><audio controls>
-                    <source src ="${yourPokemonSound}" type="audio/wav">
-                    Your Browser doesn't support the audio element.
-                    </audio></p>
-                    <p><b>================================================</b></p>
-                    <p><b>Enemy Pokemon: ${enemyPokemonName}}</b></p>
-                    <p><b>Enemy Pokemon Move Name: ${enemyPokemonMoveName}}</b></p>
-                    <p><b>Enemy Pokemon HP: ${enemyPokemonHP}}</b></p>
-                    <p><b>Enemy Pokemon DMG: ${enemyPokemonDMG}}</b></p>
-                    <p><b>Enemy Pokemon image: <img src="data:image/png;base64,${enemyPokemonImage}"></b></p>
-                    <p><audio controls>
-                    <source src ="${enemyPokemonSound}" type="audio/wav">
-                    Your Browser doesn't support the audio element.
-                    </audio></p>
-                     
-                    <p><b>================================================}</b></p>
-                    <p><b>It's a draw! No one won!}</b></p>
-                    <p><b>Head back to <a href="http://localhost:3000">http://localhost:3000</a>}</b></p>`)
+                res.json({
+                    winner:null,
+                    YourPokemon:{
+                        name: yourPokemonName,
+                        attack: yourPokemonDMG,
+                        hp: yourPokemonHP
+                    },
+                    EnemyPokemon:{
+                        name:enemyPokemonName,
+                        attack: enemyPokemonDMG,
+                        hp: enemyPokemonHP
+                    },
+                    results: "Draw!"
+                })
                     clearPokemon()
             }
     }
